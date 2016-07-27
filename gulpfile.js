17eea27f5327b51388f3c615981bbdd6ccbe8e1f
Artem Ryabkov <gold_100@bk.ru>
2016-07-27 21:03:44 +0300

    fixup! Последние штрихи

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,14 @@ var autoprefixer = require("gulp-autoprefixer");
 var plumber = require("gulp-plumber");
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
+var minify = require("gulp-csso");
 var rename = require("gulp-rename");
 var csscomb = require("gulp-csscomb");
 var server = require("browser-sync");
+var run = require("run-sequence");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+var copy = require("gulp-copy");
 
 
 var browsers = [
@@ -22,6 +27,21 @@ var browsers = [
     "ie 9"
 ];
 
+gulp.task("del", function() {
+    return del("build");
+});
+
+gulp.task("copy", function() {
+    return gulp.src([
+        "fonts/**/*.{woff,woff2}",
+        "img/**",
+        "js/**",
+        "*.html"
+    ], {
+        base: "."
+    }).pipe(gulp.dest("build"));
+});
+
 gulp.task('csscomb', function() {
     return gulp.src('css/style.css')
         .pipe(csscomb())
@@ -31,6 +51,16 @@ gulp.task('csscomb', function() {
 gulp.task("svgmin", function () {
     return gulp.src("img/*.svg")
         .pipe(svgmin())
+        .pipe(gulp.dest("build/img/"));
+});
+
+gulp.task("svgstore",["svgmin"], function () {
+    return gulp
+        .src("build/img/*.svg")
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename("spriteSvg.svg"))
         .pipe(gulp.dest("build/img/"));
 });
 
@@ -44,6 +74,26 @@ gulp.task("svgstore:dev", function () {
         .pipe(gulp.dest("img/"));
 });
 
+gulp.task("images", function() {
+    return gulp.src("build/img/**/*.{png,jpg,gif}")
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.jpegtran({progressive: true})
+        ]))
+        .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("cssbuild", function() {
+    gulp.src("less/style.less")
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(autoprefixer({browsers: browsers}))
+        .pipe(gulp.dest("build/css"))
+        .pipe(minify())
+        .pipe(rename("style.min.css"))
+        .pipe(gulp.dest("build/css"));
+});
+
 gulp.task("cssbuild:dev", function() {
     gulp.src("less/style.less")
         .pipe(plumber())
@@ -51,6 +101,16 @@ gulp.task("cssbuild:dev", function() {
         .pipe(autoprefixer({browsers: browsers}))
         .pipe(gulp.dest("css"))
         .pipe(server.reload({stream: true}));
+});
+
+gulp.task("build", function() {
+    run(
+        "del",
+        "copy",
+        "svgstore",
+        "cssbuild",
+        "images"
+    );
 });
 
 gulp.task('serve',["cssbuild:dev","svgstore:dev"], function () {
